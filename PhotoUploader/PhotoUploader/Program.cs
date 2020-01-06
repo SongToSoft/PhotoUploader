1b5b11af3178a7b0df4329018982fee2fd68ac87
Bogdanov Dmitry <bogdanovdi888@gmail.com>
2020-01-06 16:45:13 +1000

    Add project
    Add project

---
+++ PhotoUploader/PhotoUploader/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoUploader
+{
+    class Program
+    {
+        static void SetGithubAvatar()
+        {
+            WebWrapper webWrapper = new WebWrapper();
+            webWrapper.OpenSite("https://github.com/settings/profile");
+            webWrapper.FillFieldByName("login", "login");
+            webWrapper.FillFieldByName("password", "password");
+            webWrapper.ClickOnElementByName("commit");
+            webWrapper.ClickOnElementByPath("//img[@class='avatar rounded-2']/..");
+            //webWrapper.ClickOnElementByPath("//label[@class='dropdown-item text-normal']");
+
+            webWrapper.UploadFile("//input[@class='manual-file-chooser width-full ml-0 js-manual-file-chooser']", @"C:\Users\Admin\Pictures\prism runner logo\logo 72.png");
+
+            //webWrapper.ClickOnElementByClassName("btn-primary");
+            //webWrapper.ClickOnElementByPath("//button[contains(text(), 'Set new profile picture')]"); 
+            //webWrapper.ClickOnElementByName("op");
+            //webWrapper.ClickOnElementByPath("//button[contains(.,'Set new profile picture')]");
+            //webWrapper.ClickOnElementByClassName("btn.btn-primary.btn-block");
+            //webWrapper.ClickOnElementByCssSelector("btn btn-primary btn-block");
+            //webWrapper.ClickOnElementByPath("//*[contains(text(),'Set new profile picture')]");
+            //webWrapper.ClickOnElementByPath("//button[@value='save']/..");
+            //webWrapper.ClickOnElementByLinkText("      Set new profile picture    ");
+            //webWrapper.ClickOnElementByCssSelector("button.btn.btn-primary.btn-block");
+            //webWrapper.ClickOnElementByPath("//button[@value='save']/html/body/details/details-dialog/form/div[2]/button");
+            //webWrapper.ClickOnElementByCssSelector("body > details > details-dialog > form > div.Box-footer > button");
+            webWrapper.ClickOnElementByPath("//button[@type='submit'][@name='op'][@value='save'][@class='btn btn-primary btn-block']");
+        }
+
+        static void Main(string[] args)
+        {
+            SetGithubAvatar();
+            Console.WriteLine("End of programm");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PhotoUploader/PhotoUploader/WebWrapper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace PhotoUploader
+{
+    class WebWrapper
+    {
+        private ChromeDriver chromeDriver;
+
+        public WebWrapper()
+        {
+            chromeDriver = new ChromeDriver();
+        }
+
+        public void OpenSite(string url)
+        {
+            chromeDriver.Navigate().GoToUrl(url);
+        }
+
+        public void ClickOnElementByName(string elementName)
+        {
+            var element = chromeDriver.FindElement(By.Name(elementName));
+            element.Click();
+        }
+
+        public void ClickOnElementByClassName(string elementClassName)
+        {
+            var element = chromeDriver.FindElement(By.ClassName(elementClassName));
+            element.Click();
+        }
+
+        public void ClickOnElementByPath(string elementPath)
+        {
+            var element = chromeDriver.FindElement(By.XPath(elementPath));
+            element.Click();
+        }
+
+        public void ClickOnElementByCssSelector(string elementSelector)
+        {
+            var element = chromeDriver.FindElement(By.CssSelector(elementSelector));
+            element.Click();
+        }
+
+        public void ClickOnElementByLinkText(string elementText)
+        {
+            var element = chromeDriver.FindElement(By.LinkText(elementText));
+            element.Click();
+        }
+
+        public void FillFieldByName(string fieldName, string text)
+        {
+            var field = chromeDriver.FindElement(By.Name(fieldName));
+            field.Clear();
+            field.SendKeys(text);
+        }
+
+        public void FillFieldByClassName(string fieldClassName, string text)
+        {
+            var field = chromeDriver.FindElement(By.Name(fieldClassName));
+            field.Clear();
+            field.SendKeys(text);
+        }
+
+        public void UploadFile(string elementPath, string fileName)
+        {
+            //var element = chromeDriver.FindElement(By.Id("uploadhere"));
+            //element.SendKeys(fileName);
+            var fileInput = chromeDriver.FindElement(By.XPath(elementPath));
+            //var fileInput = chromeDriver.FindElement(By.Id("'avatar_upload'"));
+            fileInput.SendKeys(fileName);
+            //fileInput.Submit();
+        }
+    }
+}
